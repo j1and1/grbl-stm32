9e7eeb2316aeb82e9140794f5c89840c36ff35bc
Ed Robbins <edd.robbins@gmail.com>
2021-06-18 01:20:05 +0100

    Change the pin settings to match the olimexino with _unreal_'s adapter board

--- grbl/cpu_map_stm32.h
+++ grbl/cpu_map_stm32.h
@@ -4,23 +4,23 @@
 // Define step pulse output pins. NOTE: All step bit pins must be on the same port.
 #define STEP_PORT GPIOA
 #define RCC_STEP_PORT RCC_APB2Periph_GPIOA
-#define X_STEP_BIT 0
-#define Y_STEP_BIT 1
-#define Z_STEP_BIT 2
+#define X_STEP_BIT 3
+#define Y_STEP_BIT 2
+#define Z_STEP_BIT 0
 #define STEP_MASK ((1 << X_STEP_BIT) | (1 << Y_STEP_BIT) | (1 << Z_STEP_BIT)) // All step bits
 
 // Define step direction output pins. NOTE: All direction pins must be on the same port.
 #define DIRECTION_PORT GPIOA
 #define RCC_DIRECTION_PORT RCC_APB2Periph_GPIOA
-#define X_DIRECTION_BIT 3
-#define Y_DIRECTION_BIT 4
-#define Z_DIRECTION_BIT 5
+#define X_DIRECTION_BIT 8
+#define Y_DIRECTION_BIT 9
+#define Z_DIRECTION_BIT 10
 #define DIRECTION_MASK ((1 << X_DIRECTION_BIT) | (1 << Y_DIRECTION_BIT) | (1 << Z_DIRECTION_BIT)) // All direction bits
 
 // Define stepper driver enable/disable output pin.
 #define STEPPERS_DISABLE_PORT GPIOA
 #define RCC_STEPPERS_DISABLE_PORT RCC_APB2Periph_GPIOA
-#define STEPPERS_DISABLE_BIT 6
+#define STEPPERS_DISABLE_BIT 10
 #define STEPPERS_DISABLE_MASK (1 << STEPPERS_DISABLE_BIT)
 #define SetStepperDisableBit() GPIO_SetBits(STEPPERS_DISABLE_PORT, STEPPERS_DISABLE_MASK)
 #define ResetStepperDisableBit() GPIO_ResetBits(STEPPERS_DISABLE_PORT, STEPPERS_DISABLE_MASK)
@@ -31,20 +31,20 @@
 #define LIMIT_PORT GPIOB
 #define RCC_LIMIT_PORT RCC_APB2Periph_GPIOB
 #define GPIO_LIMIT_PORT GPIO_PortSourceGPIOB
-#define X_LIMIT_BIT 10
-#define Y_LIMIT_BIT 11
-#define Z_LIMIT_BIT 12
+#define X_LIMIT_BIT 5
+#define Y_LIMIT_BIT 6
+#define Z_LIMIT_BIT 7
 
 #define LIMIT_MASK ((1 << X_LIMIT_BIT) | (1 << Y_LIMIT_BIT) | (1 << Z_LIMIT_BIT)) // All limit bits
 
 // Define spindle enable and spindle direction output pins.
-#define SPINDLE_ENABLE_PORT GPIOB
-#define RCC_SPINDLE_ENABLE_PORT RCC_APB2Periph_GPIOB
-#define SPINDLE_ENABLE_BIT 13 //
+#define SPINDLE_ENABLE_PORT GPIOA
+#define RCC_SPINDLE_ENABLE_PORT RCC_APB2Periph_GPIOA
+#define SPINDLE_ENABLE_BIT 6 //
 #ifndef USE_SPINDLE_DIR_AS_ENABLE_PIN
-#define SPINDLE_DIRECTION_DDR GPIOB
-#define SPINDLE_DIRECTION_PORT GPIOB
-#define SPINDLE_DIRECTION_BIT 14 //
+#define SPINDLE_DIRECTION_DDR GPIOA
+#define SPINDLE_DIRECTION_PORT GPIOA
+#define SPINDLE_DIRECTION_BIT 5 //
 #endif
 #define SetSpindleEnablebit() GPIO_WriteBit(SPINDLE_ENABLE_PORT, 1 << SPINDLE_ENABLE_BIT, Bit_SET)
 #define ResetSpindleEnablebit() GPIO_WriteBit(SPINDLE_ENABLE_PORT, 1 << SPINDLE_ENABLE_BIT, Bit_RESET)
@@ -53,23 +53,23 @@
 
 // Define flood and mist coolant enable output pins.
 // a later date if flash and memory space allows.
-#define COOLANT_FLOOD_PORT GPIOB
-#define RCC_COOLANT_FLOOD_PORT RCC_APB2Periph_GPIOB
+#define COOLANT_FLOOD_PORT GPIOC
+#define RCC_COOLANT_FLOOD_PORT RCC_APB2Periph_GPIOC
 #define COOLANT_FLOOD_BIT 3
-#define COOLANT_MIST_PORT GPIOB
-#define RCC_COOLANT_MIST_PORT RCC_APB2Periph_GPIOB
+#define COOLANT_MIST_PORT GPIOC
+#define RCC_COOLANT_MIST_PORT RCC_APB2Periph_GPIOC
 #define COOLANT_MIST_BIT 4
 
 // Define user-control controls (cycle start, reset, feed hold) input pins.
 // NOTE: All CONTROLs pins must be on the same port and not on a port with other input pins (limits).
-#define CONTROL_PIN_PORT GPIOB
-#define CONTROL_PORT GPIOB
-#define RCC_CONTROL_PORT RCC_APB2Periph_GPIOB
-#define GPIO_CONTROL_PORT GPIO_PortSourceGPIOB
-#define CONTROL_RESET_BIT 5
-#define CONTROL_FEED_HOLD_BIT 6
-#define CONTROL_CYCLE_START_BIT 7
-#define CONTROL_SAFETY_DOOR_BIT 8
+#define CONTROL_PIN_PORT GPIOC
+#define CONTROL_PORT GPIOC
+#define RCC_CONTROL_PORT RCC_APB2Periph_GPIOC
+#define GPIO_CONTROL_PORT GPIO_PortSourceGPIOC
+#define CONTROL_RESET_BIT 0
+#define CONTROL_FEED_HOLD_BIT 1
+#define CONTROL_CYCLE_START_BIT 2
+#define CONTROL_SAFETY_DOOR_BIT 5
 #define CONTROL_MASK ((1 << CONTROL_RESET_BIT) | (1 << CONTROL_FEED_HOLD_BIT) | (1 << CONTROL_CYCLE_START_BIT) | (1 << CONTROL_SAFETY_DOOR_BIT))
 
 // Define probe switch input pin.
@@ -86,7 +86,7 @@
 #define SPINDLE_PWM_DDR GPIOA
 #define SPINDLE_PWM_PORT GPIOA
 #define RCC_SPINDLE_PWM_PORT RCC_APB2Periph_GPIOA
-#define SPINDLE_PWM_BIT 8
+#define SPINDLE_PWM_BIT 6
 #endif // End of VARIABLE_SPINDLE
 #define SPINDLE_PWM_MAX_VALUE (1000000 / SPINDLE_PWM_FREQUENCY)
 #ifndef SPINDLE_PWM_MIN_VALUE
